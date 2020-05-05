3251b60d124d5e8da42838800f7f78284766f17a
ingenie <cubya0104@gmail.com>
2020-05-05 14:45:45 +0900

    authenticationJwt

--- src/passport.js
+++ src/passport.js
@@ -3,24 +3,24 @@ import { Strategy, ExtractJwt } from "passport-jwt";
 import { prisma } from "../generated/prisma-client";
 
 const jwtOptions = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: process.env.JWT_SECRET
-  };
-
-const verifyUser = async (payload, done) => {
-    try {
-        const user = await prisma.user({ id: payload.id });
-        if (user !== null) {
-        return done(null, user);
-        } else {
-        return done(null, false);
-        }
-    } catch (error) {
-        return done(error, false);
-    }
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET
 };
 
-export const authenticateJwt = (req, res, next) =>
+const verifyUser = async (payload, done) => {
+  try {
+    const user = await prisma.user({ id: payload.id });
+    if (user !== null) {
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
+  } catch (error) {
+    return done(error, false);
+  }
+};
+
+export const authenticationJwt = (req, res, next) =>
   passport.authenticate("jwt", { sessions: false }, (error, user) => {
     if (user) {
       req.user = user;
@@ -28,6 +28,5 @@ export const authenticateJwt = (req, res, next) =>
     next();
   })(req, res, next);
 
-  
 passport.use(new Strategy(jwtOptions, verifyUser));
 passport.initialize();
--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import schema from "./schema";
 import "./passport";
 import { authenticationJwt} from "./passport";
 import { isAuthenticated} from "./middlewares";
-//import {uploadMiddleware, uploadController} from "./upload";
+import {uploadMiddleware, uploadController} from "./upload";
 
 const PORT = process.env.PORT || 4000;
 
@@ -15,8 +15,8 @@ const server = new GraphQLServer({
 });
 
 server.express.use(logger("dev"));
-//server.express.use(authenticationJwt);
-//server.express.post("/api/upload", uploadMiddleware, uploadController);
+server.express.use(authenticationJwt);
+server.express.post("/api/upload", uploadMiddleware, uploadController);
 
 server.start({ port : PORT}, () => 
     console.log(`✅ Server running on http://localhost:${PORT}`)
